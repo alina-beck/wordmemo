469dc689a221e4fded1bc5d7b9fdd260adfc78c7
Alina Beck <alina.beck@mail.com>
2016-03-20 15:59:10 +0100

    compares user input to valid translation, forwards to respective view (wrong/right)

--- app/src/main/java/app/wordmemo/PracticeActivity.java
+++ app/src/main/java/app/wordmemo/PracticeActivity.java
@@ -3,6 +3,7 @@ package app.wordmemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.ViewFlipper;
 
@@ -13,24 +14,42 @@ public class PracticeActivity extends AppCompatActivity {
 
     List<Word> dueWords;
     TextView originalDueWordView;
+    Word currentDueWord;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practice);
+
         WordController wordController = new WordController();
         this.dueWords = wordController.getDueWords();
         this.originalDueWordView = (TextView) findViewById(R.id.originalDueWord);
-        displayOriginalDueWord();
+        setCurrentDueWord();
     }
 
-    public void displayOriginalDueWord () {
-        Word randomWord = dueWords.get(new Random().nextInt(dueWords.size()));
-        originalDueWordView.setText(randomWord.getOriginal());
+    public void setCurrentDueWord () {
+        this.currentDueWord = dueWords.get(new Random().nextInt(dueWords.size()));
+        originalDueWordView.setText(currentDueWord.getOriginal());
     }
 
-    public void changeView (View view) {
+    public void validateUserTranslation (View view) {
+        EditText editText = (EditText) findViewById(R.id.userTranslation);
+        String userTranslation = editText.getText().toString();
+        String validTranslation = currentDueWord.getTranslation();
+
+        boolean isTranslationRight = userTranslation.equalsIgnoreCase(validTranslation);
+
+        changeView(isTranslationRight);
+    }
+
+    public void changeView (boolean isTranslationRight) {
         ViewFlipper flip = (ViewFlipper) findViewById(R.id.practiceViewFlipper);
-        flip.showNext();
+
+        if (isTranslationRight) {
+            flip.setDisplayedChild(flip.indexOfChild(findViewById(R.id.practiceRight)));
+        }
+        else {
+            flip.setDisplayedChild(flip.indexOfChild(findViewById(R.id.practiceWrong)));
+        }
     }
 }
