d09a5d439f59aa3fb6e231624adf4eae1c899d1a
Alina Beck <alina.beck@mail.com>
2016-03-24 21:41:03 +0100

    added MenuFragment to WelcomeActivity

--- app/src/main/java/app/wordmemo/activities/WelcomeActivity.java
+++ app/src/main/java/app/wordmemo/activities/WelcomeActivity.java
@@ -1,13 +1,12 @@
 package app.wordmemo.activities;
 
 import android.content.Intent;
-import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 import app.wordmemo.R;
-import app.wordmemo.databinding.ActivityWelcomeBinding;
+import app.wordmemo.fragments.MenuFragment;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -16,9 +15,16 @@ public class WelcomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
 
-        ActivityWelcomeBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_welcome);
-        String testString = "Open Practice";
-        binding.setTest(testString);
+        if (findViewById(R.id.menu_fragment_container) != null) {
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            MenuFragment menuFragment = new MenuFragment();
+
+            getSupportFragmentManager().beginTransaction().add(R.id.menu_fragment_container, menuFragment).commit();
+        }
+
     }
 
     public void startPractice(View view) {
