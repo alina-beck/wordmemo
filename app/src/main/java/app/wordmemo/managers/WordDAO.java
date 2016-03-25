f23d5a97f998130a4582149fc999d6680fe03830
Alina Beck <alina.beck@mail.com>
2016-03-25 17:54:44 +0100

    created WordDAO, insertWord method

--- app/src/main/java/app/wordmemo/managers/WordDAO.java
+++ app/src/main/java/app/wordmemo/managers/WordDAO.java
@@ -1,6 +1,49 @@
 package app.wordmemo.managers;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import app.wordmemo.models.Word;
+import app.wordmemo.utils.DateUtil;
+
 public class WordDAO {
 
+    private static WordDAO sInstance;
+
+    private SQLiteDatabase wordDatabase;
+    private WordDbHelper wordDbHelper;
+
+    private WordDAO(Context context) {
+        wordDbHelper = new WordDbHelper(context);
+    }
+
+    public static synchronized WordDAO getInstance(Context context) {
+        if (sInstance == null) {
+            sInstance = new WordDAO(context);
+        }
+        return sInstance;
+    }
+
+    public void open() {
+        wordDatabase = wordDbHelper.getWritableDatabase();
+    }
+
+    public void close() {
+        wordDbHelper.close();
+    }
+
+    public boolean insertWord (Word word) {
+        ContentValues values = new ContentValues();
+        values.put(WordDbHelper.COLUMN_ID, word.getId());
+        values.put(WordDbHelper.COLUMN_ORIGINAL, word.getOriginal());
+        values.put(WordDbHelper.COLUMN_TRANSLATION, word.getTranslation());
+        values.put(WordDbHelper.COLUMN_DUEDATE, DateUtil.formatDate(word.getDueDate()));
+
+        long result = wordDatabase.insert(WordDbHelper.TABLE_WORDS, null, values);
+
+        return result != -1;
+
+    }
 
 }
